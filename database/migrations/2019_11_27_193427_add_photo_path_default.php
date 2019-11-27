3e0b36084a5b87e68854adcc12eea7f6e8b17dd4
Aladin <42503661+Aladin96@users.noreply.github.com>
2019-11-27 20:39:13 +0100

    Change Column photo by adding Default value

---
+++ database/migrations/2019_11_27_193427_add_photo_path_default.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddPhotoPathDefault extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('candidats', function (Blueprint $table) {
+          $table->string('photo')->default('assets/images/default-profile.jpg')->change();
+          $table->string('in')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('candidats', function (Blueprint $table) {
+            //
+        });
+    }
+}
