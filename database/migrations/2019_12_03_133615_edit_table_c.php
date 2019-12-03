e05bbd517e4c457002e0f0e6c2ce1c6b4aadcee9
youcefkh <youcefkh13.yk@gmail.com>
2019-12-03 14:50:23 +0100

    Fixed some SQL Errors

---
+++ database/migrations/2019_12_03_133615_edit_table_c.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class EditTableC extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('candidats', function (Blueprint $table) {
+            //
+            $table->string('photo')->default('assets/images/default-profile.jpg')->change();
+            $table->string('in')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('candidats', function (Blueprint $table) {
+            //
+        });
+    }
+}
